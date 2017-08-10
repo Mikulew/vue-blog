553010274f53978dbec852af3874dd36cc49d803
Mikulew <mikulew@gmail.com>
2017-08-10 18:20:13 +0200

    2nd commit - dist folder

--- src/components/singleBlog.vue
+++ src/components/singleBlog.vue
@@ -2,11 +2,13 @@
   <div id="single-blog">
     <h1>{{blog.title | to-uppercase}}</h1>
     <article>{{blog.content}}</article>
-    <p>Author: {{ blog.author }}</p>
-    <p>Categories:</p>
-    <ul>
-        <li v-for="category in blog.categories">{{ category }}</li>
-    </ul>
+    <div class="single-blog-wrapper">
+      <p class="single-blog-author">Author: {{ blog.author }}</p>
+      <p class="single-blog-categories">Categories:</p>
+      <ul>
+          <li v-for="category in blog.categories">{{ category }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -32,5 +34,23 @@ export default {
   #single-blog {
     max-width: 960px;
     margin: 0 auto;
+  }
+  .single-blog-wrapper {
+    display: flex;
+    border: 1px solid #eee;
+    padding: 10px;
+  }
+  .single-blog-categories {
+    margin: 10px 5px;
+  }
+  .single-blog-author {
+    margin: 10px;
+  }
+  ul {
+    display: inline-flex;
+    margin: 10px 0;
+  }
+  li {
+    list-style-type: none;
   }
 </style>
